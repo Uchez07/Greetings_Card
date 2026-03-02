d3700e601727b158d3aade34fae766b92a69d5ad
Gacheru <johngacheru533@gmail.com>
2026-03-02 15:10:10 +0300

    change of url

--- Cards/urls.py
+++ Cards/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path("card/", views.view_card, name="view_card")
+    path("", views.view_card, name="view_card")
 ]
