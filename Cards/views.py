2dd588dc026aa47d65726d4b4b68c128b471634e
Gacheru <johngacheru533@gmail.com>
2026-03-02 15:16:35 +0300

    updated the views path

--- Cards/views.py
+++ Cards/views.py
@@ -5,10 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Card
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("<h1>Welcome to My Greeting App 🎉</h1>")
-
 def view_card(request):
     name = request.GET.get('name', 'Serena, My Love')
     message = request.GET.get('msg', 'Love')
-    return render(request, 'cards/card.html', {'card': {'recipient_name': name, 'message': message}})
+    return render(request, 'Cards/card.html', {'card': {'recipient_name': name, 'message': message}})
